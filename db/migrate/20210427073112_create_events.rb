82787fe587bfae6f41fabebc0c5fe0b959ba6e03
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-04-27 10:32:34 +0300

    event model generated and migrated

---
+++ db/migrate/20210427073112_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.string :date
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_115436) do
+ActiveRecord::Schema.define(version: 2021_04_27_073112) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "date"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
